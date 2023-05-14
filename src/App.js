80b1f9bf83833110785bb330e2f3f8d99b91ea1d
Zee <jzivkovic@nextlinklabs.com>
2023-05-14 17:26:04 -0600

    Convert to use yarn instead of npm

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Will this push to Amplify?</h1>
+        <h1>Using yarn instead of npm</h1>
       </header>
     </div>
   );
