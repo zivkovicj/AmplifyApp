27bbb73eb9edeb4699e2701702335cc9f4640c94
Zee <jzivkovic@nextlinklabs.com>
2023-05-14 17:43:11 -0600

    Updated build file on Amplify.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Using yarn instead of npm</h1>
+        <h1>Changed the build file in Amplify.</h1>
       </header>
     </div>
   );
